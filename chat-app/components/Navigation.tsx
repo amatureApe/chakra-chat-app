0d876757b841c6500b01ce35a5b8fa8dd2bd2587
amatureApe <amatureApe@gmail.com>
2022-10-24 14:42:25 -0500

    add all navbar buttons

--- chat-app/components/Navigation.tsx
+++ chat-app/components/Navigation.tsx
@@ -1,6 +1,6 @@
-import { VStack, IconButton, Tooltip } from '@chakra-ui/react'
+import { VStack, IconButton, Tooltip, Icon } from '@chakra-ui/react'
 import { MdDashboard, MdMail, MdSettings } from 'react-icons/md'
-import { HiLightningBolt, HiBell, HiTag, HiSearch } from 'react-icons/ui'
+import { HiLightningBolt, HiBell, HiTag, HiSearch } from 'react-icons/hi'
 
 const Navigation = () => {
   return (
@@ -10,7 +10,25 @@ const Navigation = () => {
         <Tooltip label="Dashboard" placement="right">
           <IconButton color="gray.500" icon={<MdDashboard />} aria-label="Dashboard" />
         </Tooltip>
+        <Tooltip label="Actions" placement="right">
+          <IconButton color="gray.500" icon={<HiLightningBolt />} aria-label="Actions" />
+        </Tooltip>
+        <Tooltip label="Search" placement="right">
+          <IconButton color="gray.500" icon={<HiSearch />} aria-label="Search" />
+        </Tooltip>
+        <Tooltip label="Notifications" placement="right">
+          <IconButton color="gray.500" icon={<HiBell />} aria-label="Notifications" />
+        </Tooltip>
+        <Tooltip label="Tags" placement="right">
+          <IconButton color="gray.500" icon={<HiTag />} aria-label="Tags" />
+        </Tooltip>
+        <Tooltip label="Messages" placement="right">
+          <IconButton color="gray.500" icon={<MdMail />} aria-label="Messages" />
+        </Tooltip>
       </VStack>
+      <Tooltip label="Settings" placement="right">
+        <IconButton color="gray.500" icon={<MdSettings />} aria-label="Settings" />
+      </Tooltip>
     </VStack>
   )
 }
