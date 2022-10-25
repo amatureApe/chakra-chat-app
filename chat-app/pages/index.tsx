c2dd05871528aa4fb352b15a4c95a9166c5468f3
amatureApe <amatureApe@gmail.com>
2022-10-24 19:16:32 -0500

    add Chat with ChatBubbles

--- chat-app/pages/index.tsx
+++ chat-app/pages/index.tsx
@@ -1,8 +1,8 @@
 import { HStack, Flex } from '@chakra-ui/react'
-import { RiDribbbleLine, RiInstagramLine, RiTwitterFill } from 'react-icons/ri'
 
 import Navigation from '../components/Navigation'
 import ChatHistorySidebar from '../components/ChatHistory/ChatHistorySidebar'
+import Chat from '../components/Chat'
 
 const IndexPage = () => (
   <HStack h="100vh" spacing={0}>
@@ -12,7 +12,9 @@ const IndexPage = () => (
     <Flex as="aside" h="full" maxW="sm" w="full" borderRightColor="gray.100" borderRightWidth={1} pt={8}>
       <ChatHistorySidebar />
     </Flex>
-    <Flex as="main" h="full" flex={1} borderRightColor="gray.100" borderRightWidth={1} />
+    <Flex as="main" h="full" flex={1} borderRightColor="gray.100" borderRightWidth={1}>
+      <Chat />
+    </Flex>
     <Flex as="aside" h="full" maxW="sm" w="full" />
   </HStack>
 )
